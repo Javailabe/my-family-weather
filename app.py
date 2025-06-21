90165f21b91ce6b22be45c2b9ba01674315a5b7c
Javailabe <kulecki.pawel@gmail.com>
2025-06-21 22:57:11 +0200

    alignment with github

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import json
+import requests
+import pandas as pd
+import xml.etree.ElementTree as ET
+import sys
 
-cities_path = "cities-example.json"
-
-def get_cities_data(file):
+def get_data_from_cities():
+    file = "cities-example.json"
     with open(file, "r", encoding="utf-8") as input:
-        cities = json.load(input)
-    return cities
+        data = json.load(input)
+        return data
 
-cities = get_cities_data("cities-example.json")
+def get_forecast(lat, lon):
+    url = f"https://api.met.no/weatherapi/locationforecast/2.0/classic?lat={lat}&lon={lon}"
+    
+
+cities = get_data_from_cities()
+data = get_forecast(59.9139, 10.7522)
 print(cities)
